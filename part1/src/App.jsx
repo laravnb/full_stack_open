955b51b9488636ce1c364df3c236561e9e61d369
Lara <lara.van.breda@gmail.com>
2026-08-03 10:39:04 +0200

    ex1.7: unicafe feedback average feedback

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -2,21 +2,48 @@ import { useState } from "react";
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAllClicks] = useState(0);
 
   const handleGoodClick = () => {
     setGood(good + 1);
+    setAllClicks(all + 1);
   };
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
+    setAllClicks(all + 1);
   };
 
   const handleBadClick = () => {
     setBad(bad + 1);
+    setAllClicks(all + 1);
+  };
+
+  const Average = () => {
+    if (all === 0) {
+      return 0;
+    }
+    const Average = (good * 1 + neutral * 0 + bad * -1) / all;
+    return Average;
+  };
+
+  const PositiveFeedback = () => {
+    if (all === 0) {
+      return 0;
+    }
+    const positiveFeedback = (good / all) * 100;
+    return positiveFeedback.toPrecision(14);
   };
 
   return (
@@ -27,9 +54,12 @@ const App = () => {
       <Button onClick={handleBadClick} text="bad" />
 
       <h1>statistics</h1>
-      <div>good: {good}</div>
-      <div>neutral: {neutral}</div>
-      <div>bad: {bad}</div>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {Average()}</div>
+      <div>positive {PositiveFeedback()} % </div>
     </div>
   );
 };
