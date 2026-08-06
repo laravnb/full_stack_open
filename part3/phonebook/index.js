d919912b8dda7c0f713f5dabc43bbc3595d234ab
Lara <lara.van.breda@gmail.com>
2026-08-06 17:04:28 +0200

    ex3.9: add single-page app usage from backend address

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
@@ -8,6 +9,8 @@ morgan.token("body", (request) => JSON.stringify(request.body));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
+app.use(cors());
+app.use(express.static("dist"));
 
 let entries = [
   {
@@ -99,7 +102,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
