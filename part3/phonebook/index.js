0cf4db90e68f1b5349c1c846a9469f941d2214ce
Lara <lara.van.breda@gmail.com>
2026-08-06 11:31:26 +0200

    ex3.3: add endpoint to retrieve person by ID

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -36,6 +36,16 @@ app.get("/api/info", (request, response) => {
   response.send(`Phonebook has info for ${amount} people<br>${time}`);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = entries.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
