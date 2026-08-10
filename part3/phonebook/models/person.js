8a943a592294a36172087dbb7a946ad2202b79c2
Lara <lara.van.breda@gmail.com>
2026-08-10 15:01:20 +0200

    ex3.22: add ESlint

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require( 'mongoose ') 
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -11,14 +11,14 @@ const personSchema = new mongoose.Schema({
     minLength: 8,
     required: true,
   },
-});
+}) 
 
-personSchema.set("toJSON", {
+personSchema.set( 'toJSON ', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString() 
+    delete returnedObject._id 
+    delete returnedObject.__v 
   },
-});
+}) 
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model( 'Person ', personSchema) 
--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -1,18 +1,16 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
+require( 'dotenv ').config() 
+const mongoose = require( 'mongoose ') 
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+  console.log( 'give password as argument ') 
+  process.exit(1) 
 }
 
-const password = process.argv[2];
+const url = process.env.MONGODB_URI 
 
-const url = process.env.MONGODB_URI;
+mongoose.set( 'strictQuery ', false) 
 
-mongoose.set("strictQuery", false);
-
-mongoose.connect(url, { family: 4 });
+mongoose.connect(url, { family: 4 }) 
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -25,33 +23,33 @@ const personSchema = new mongoose.Schema({
     minLength: 8,
     required: true,
   },
-});
+}) 
 
-const Person = mongoose.model("Person", personSchema);
+const Person = mongoose.model( 'Person ', personSchema) 
 
 if (process.argv.length === 3) {
   Person.find({}).then((result) => {
     result.forEach((person) => {
-      console.log(`${person.name} ${person.number}`);
-    });
-    mongoose.connection.close();
-  });
+      console.log(`${person.name} ${person.number}`) 
+    }) 
+    mongoose.connection.close() 
+  }) 
 } else if (process.argv.length === 5) {
-  const phonebook_name = process.argv[3];
-  const phonebook_number = process.argv[4];
+  const phonebook_name = process.argv[3] 
+  const phonebook_number = process.argv[4] 
 
   const person = new Person({
     name: phonebook_name,
     number: phonebook_number,
-  });
+  }) 
 
-  person.save().then((result) => {
+  person.save().then(() => {
     console.log(
       `Added ${phonebook_name} number ${phonebook_number} to phonebook`,
-    );
-    mongoose.connection.close();
-  });
+    ) 
+    mongoose.connection.close() 
+  }) 
 } else {
-  console.log("Please provide password <name> <number>");
-  mongoose.connection.close();
+  console.log( 'Please provide password <name> <number> ') 
+  mongoose.connection.close() 
 }
