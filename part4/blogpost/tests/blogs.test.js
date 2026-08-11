62db5ea78236c016946c9327a3c1de86e45562f1
Lara <lara.van.breda@gmail.com>
2026-08-11 11:28:00 +0200

    ex4.4: add tests for totalLikes function in list_helper

--- part4/blogpost/tests/blogs.test.js
+++ part4/blogpost/tests/blogs.test.js
@@ -8,3 +8,52 @@ test("dummy returns one", () => {
   const result = listHelper.dummy(blogs);
   assert.strictEqual(result, 1);
 });
+
+describe("total likes", () => {
+  test("of emmpty list is zero", () => {
+    const blogs = [];
+
+    const result = listHelper.totalLikes(blogs);
+    assert.strictEqual(result, 0);
+  });
+
+  test("when list has only one blog equals the likes of that", () => {
+    const blogs = [
+      {
+        title: "the one we went to belgium",
+        author: "kowloon and friends",
+        url: "https://simon_youtube",
+        likes: 8,
+      },
+    ];
+
+    const result = listHelper.totalLikes(blogs);
+    assert.strictEqual(result, 8);
+  });
+
+  test("of a bigger list is calculated right", () => {
+    const blogs = [
+      {
+        title: "the one we went to belgium",
+        author: "kowloon and friends",
+        url: "https://simon_youtube",
+        likes: 8,
+      },
+      {
+        title: "the one we meditated",
+        author: "friends",
+        url: "https://hindu_chants",
+        likes: 4,
+      },
+      {
+        title: "the one we coworked",
+        author: "kowloon and friends",
+        url: "https://rise_and_grind",
+        likes: 6,
+      },
+    ];
+
+    const result = listHelper.totalLikes(blogs);
+    assert.strictEqual(result, 18);
+  });
+});
