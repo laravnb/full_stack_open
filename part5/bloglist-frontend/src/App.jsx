8a156279b07ec36bf443ff231b207f156f7b8418
Lara <lara.van.breda@gmail.com>
2026-08-20 15:28:03 +0200

    ex5.10: sort blogs by likes

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -97,9 +97,11 @@ const App = () => {
           <Togglable buttonLabel="create new blog" ref={blogFormRef}>
             <BlogForm createBlog={addBlog} />
           </Togglable>
-          {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} addLike={addLike} />
-          ))}
+          {[...blogs]
+            .sort((a, b) => b.likes - a.likes)
+            .map((blog) => (
+              <Blog key={blog.id} blog={blog} addLike={addLike} />
+            ))}
         </div>
       )}
     </div>
