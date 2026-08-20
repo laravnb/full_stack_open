adf44b1b7b3022ae9a782f74669aed294df10bff
Lara <lara.van.breda@gmail.com>
2026-08-20 14:59:55 +0200

    ex5.7: add view/ hiide toggle for blog details

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={toggleVisibility}>{visible ? "hide" : "view"}</button>
+      </div>
+
+      {visible && (
+        <div>
+          <div> {blog.url} </div>
+          <div>
+            {" "}
+            likes {blog.likes} <button>like</button>{" "}
+          </div>
+          <div> {blog.author} </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
